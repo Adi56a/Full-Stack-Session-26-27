94d05d5bb64dfd5d4f9a4cd73216231fed979acc
Aditya <adityajogdand15@gmail.com>
2026-07-01 19:59:07 +0530

    1-7-26

---
+++ Full-Stack-Sessions/javascript/1-7-26/script.js
@@ -0,0 +1,38 @@
+// For Each loop runs only on the array
+
+// let marks = [60, 90, 50 , 95, 80]
+
+// marks.forEach(function(values ,index){
+
+
+//     console.log(values  - 10)
+// })
+
+
+
+
+
+// objects
+
+// let a  = {  
+// Name : "Yash", 
+// age:20,
+// marks : {
+//     "math":80,
+//     "bio":90,
+//     "web":95
+// }
+// }
+
+
+// console.log(a.marks.math)
+
+
+
+
+
+
+
+
+
+
